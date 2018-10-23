77b363f6d0767003238e0ebfde32671261b90af3
danilomartins <dglmartins@outlook.com>
2018-10-23 12:06:26 -0400

    handle post requests

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -53,6 +53,16 @@ defmodule Servy.Handler do
     |> handle_file(conv)
   end
 
+  # name=Baloo&type=Brown
+  def route(%Conv{method: "POST", path: "/bears"} = conv) do
+    # params = %{"name" => "Baloo", "type" => "Brown"}
+    %{
+      conv
+      | status: 201,
+        resp_body: "Create a #{conv.params["type"]} bear named #{conv.params["name"]}!"
+    }
+  end
+
   def route(%Conv{method: "GET", path: "/about"} = conv) do
     Path.expand("../../pages", __DIR__)
     |> Path.join("about.html")
@@ -222,6 +232,21 @@ Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
 
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+POST /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 21
+
+name=Baloo&type=Brown
 """
 
 response = Servy.Handler.handle(request)
