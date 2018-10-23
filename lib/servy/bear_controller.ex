b011cfbddea2e71776093e100c6a120eb1dea816
danilomartins <dglmartins@outlook.com>
2018-10-23 17:01:59 -0400

    comprehentions and views

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -1,28 +1,36 @@
 defmodule Servy.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
-
-  defp bear_item(bear) do
-    "<li>#{bear.name} - #{bear.type}</li>"
-  end
+  import Servy.View, only: [render: 3]
+  # alias Servy.BearView
 
   def index(conv) do
-    items =
+    bears =
       Wildthings.list_bears()
-      |> Enum.filter(&Bear.is_grizzly?/1)
       |> Enum.sort(&Bear.order_asc_by_name/2)
-      |> Enum.map(&bear_item/1)
-      |> Enum.join()
 
-    # TODO: transform bears to an HTML list
-
-    %{conv | status: 200, resp_body: "<ul>#{items}</ul>"}
+    render(conv, "index.html.eex", bears: bears)
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
-    %{conv | status: 200, resp_body: "<h1>Bear #{bear.id}: #{bear.name}</h1>"}
+
+    render(conv, "show.html.eex", bear: bear)
   end
+
+  # def index(conv) do
+  #   bears =
+  #     Wildthings.list_bears()
+  #     |> Enum.sort(&Bear.order_asc_by_name/2)
+  #
+  #   %{conv | status: 200, resp_body: BearView.index(bears)}
+  # end
+  #
+  # def show(conv, %{"id" => id}) do
+  #   bear = Wildthings.get_bear(id)
+  #
+  #   %{conv | status: 200, resp_body: BearView.show(bear)}
+  # end
 
   def create(conv, %{"name" => name, "type" => type} = params) do
     %{
