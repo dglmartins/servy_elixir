60982b15a20426ba6a4401c2a1263b39a32462a6
danilomartins <dglmartins@outlook.com>
2018-10-24 13:50:11 -0400

    concurrent, isolated processes

--- lib/servy/http_client.ex
+++ lib/servy/http_client.ex
@@ -1,17 +1,9 @@
 defmodule Servy.HttpClient do
-  def send_request(port) do
+  def send_request(request) do
     some_host_in_net = 'localhost'
 
-    request = """
-    GET /bears HTTP/1.1\r
-    Host: example.com\r
-    User-Agent: ExampleBrowser/1.0\r
-    Accept: */*\r
-    \r
-    """
-
     {:ok, socket} =
-      :gen_tcp.connect(some_host_in_net, port, [:binary, packet: :raw, active: false])
+      :gen_tcp.connect(some_host_in_net, 4000, [:binary, packet: :raw, active: false])
 
     :ok = :gen_tcp.send(socket, request)
     {:ok, response} = :gen_tcp.recv(socket, 0)
