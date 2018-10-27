2b1b15c3363359c174e3292e5a7429acef007a5a
danilomartins <dglmartins@outlook.com>
2018-10-26 21:51:10 -0400

    refactor hand made servers to GenServer

--- lib/servy/four_oh_four_counter.ex
+++ lib/servy/four_oh_four_counter.ex
@@ -1,81 +1,46 @@
-defmodule Servy.GenericServerTwo do
-  def start(callback_module, initial_state, name) do
-    pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
-    Process.register(pid, name)
-    pid
-  end
-
-  def call(pid, message) do
-    send(pid, {:call, self(), message})
-
-    receive do
-      {:response, response} -> response
-    end
-  end
-
-  def cast(pid, message) do
-    send(pid, {:cast, message})
-  end
-
-  def listen_loop(state, callback_module) do
-    receive do
-      {:call, sender, message} when is_pid(sender) ->
-        {response, new_state} = callback_module.handle_call(message, state)
-        send(sender, {:response, response})
-        listen_loop(new_state, callback_module)
-
-      {:cast, message} ->
-        new_state = callback_module.handle_cast(message, state)
-        listen_loop(new_state, callback_module)
-
-      unexpected ->
-        IO.puts("Unexpected message: #{inspect(unexpected)}")
-        listen_loop(state, callback_module)
-    end
-  end
-end
-
 defmodule Servy.FourOhFourCounter do
-  alias Servy.GenericServerTwo
+  use GenServer
 
   @process_name :four_oh_four_counter
 
   def start() do
     IO.puts("Starting the 404 counter...")
-    GenericServerTwo.start(__MODULE__, %{}, @process_name)
+    GenServer.start(__MODULE__, %{}, name: @process_name)
   end
 
   def bump_count(path) do
-    GenericServerTwo.call(@process_name, {:bump_count, path})
+    GenServer.call(@process_name, {:bump_count, path})
   end
 
   def get_count(path) do
-    GenericServerTwo.call(@process_name, {:get_count, path})
+    GenServer.call(@process_name, {:get_count, path})
   end
 
   def get_counts() do
-    GenericServerTwo.call(@process_name, :get_counts)
+    GenServer.call(@process_name, :get_counts)
   end
 
   def reset() do
-    GenericServerTwo.cast(@process_name, :reset)
+    GenServer.cast(@process_name, :reset)
   end
 
-  def handle_call({:bump_count, path}, state) do
+  # Server Callbacks
+
+  def handle_call({:bump_count, path}, _from, state) do
     new_state = Map.update(state, path, 1, &(&1 + 1))
-    {:ok, new_state}
+    {:reply, :ok, new_state}
   end
 
-  def handle_call({:get_count, path}, state) do
+  def handle_call({:get_count, path}, _from, state) do
     count = Map.get(state, path, 0)
-    {count, state}
+    {:reply, count, state}
   end
 
-  def handle_call(:get_counts, state) do
-    {state, state}
+  def handle_call(:get_counts, _from, state) do
+    {:reply, state, state}
   end
 
-  def handle_cast(:reset, state) do
-    %{}
+  def handle_cast(:reset, _state) do
+    {:noreply, %{}}
   end
 end
